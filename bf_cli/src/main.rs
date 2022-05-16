23d94d2349278164e54aa1b80bcad6d1839b009b
mystique09 <knightkrypto7@gmail.com>
2022-05-16 13:23:32 +0800

    add: main binary

--- bf_cli/src/main.rs
+++ bf_cli/src/main.rs
@@ -1,3 +1,29 @@
+use bf_lib::BrainFuck;
+use std::fs;
+
+const HELP_MESSAGE: &'static str = r#"
+Usage:
+  brainfck <option> <filename>
+
+Option:
+  -h Output this message.
+"#;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arg = std::env::args();
+    arg.next();
+
+    match arg.next() {
+        Some(n) => match n.as_str() {
+            "-h" => println!("{}", HELP_MESSAGE),
+            _ => {
+                let f = fs::read_to_string(n).expect("File not found");
+                let mut bf = BrainFuck::new(f.trim().to_string());
+                bf.exec();
+                println!("{}", bf.result);
+                return;
+            }
+        },
+        None => println!("{}", HELP_MESSAGE),
+    }
 }
